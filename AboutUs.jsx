296aa62e123d052b01e2d7c2d43baca490aae0d8
Asiphe Cetywayo <112686126+AsipheCTYWY@users.noreply.github.com>
2026-08-24 10:23:05 +0200

    Create AboutUs component for nursery information
    This component provides information about Paradise Nursery and its mission.

---
+++ AboutUs.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>About Paradise Nursery</h1>
+
+      <p>
+        Welcome to Paradise Nursery, your one-stop destination for beautiful
+        indoor and outdoor plants. We specialize in providing a wide variety of
+        healthy houseplants that bring life and freshness to your home.
+      </p>
+
+      <p>
+        Our mission is to make plant ownership enjoyable and accessible for
+        everyone. Whether you're a beginner or an experienced gardener, we have
+        the perfect plants for you.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
