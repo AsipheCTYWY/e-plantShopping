3ddcd78fca251df61e81ece329debd31f86d35c4
Asiphe Cetywayo <112686126+AsipheCTYWY@users.noreply.github.com>
2026-08-24 19:34:24 +0200

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,38 +1,44 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import ProductList from "./components/ProductList";
-import CartItem from "./components/CartItem";
-import AboutUs from "./components/AboutUs";
-import "./App.css";
 
-function LandingPage() {
+import React, { useState } from 'react';
+import ProductList from './ProductList';
+import './App.css';
+import AboutUs from './AboutUs';
+
+function App() {
+  
+  const [showProductList, setShowProductList] = useState(false);
+
+  const handleGetStartedClick = () => {
+    setShowProductList(true);
+  };
+
   return (
-    <div className="landing-page">
-      <div className="landing-content">
-        <h1>Paradise Nursery</h1>
-        <p>Your favorite online plant shop</p>
-
-        <Link to="/plants">
-          <button className="get-started-btn">
+    <div className="app-container">
+      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
+        <div className="background-image"></div>
+        <div className="content">
+         <div className="landing_content">
+         <h1>Welcome To Paradise Nursery</h1>
+          <div className="divider"></div>
+          <p>Where Green Meets Serenity</p>
+         
+          <button className="get-started-button" onClick={handleGetStartedClick}>
             Get Started
           </button>
-        </Link>
+         </div>
+          <div className="aboutus_container">
+          <AboutUs/>
+          </div>
+          </div>
+
+      </div>
+      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
+        <ProductList />
       </div>
     </div>
   );
 }
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/plants" element={<ProductList />} />
-        <Route path="/cart" element={<CartItem />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
 export default App;
+
+
